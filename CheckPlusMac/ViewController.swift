2787a6dbb575bf65616779f3573eb4ba0fd56495
Lee Angioletti <lausangio@me.com>
2016-07-08 15:29:51 -0400

    Initial Commit

---
+++ CheckPlusMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CheckPlusMac
+//
+//  Created by Lee Angioletti on 7/8/16.
+//  Copyright © 2016 Lee Angioletti. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
